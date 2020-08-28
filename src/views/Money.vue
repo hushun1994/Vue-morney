b0baab822706b6eb353a77de2ce6b68629028a73
hushun1214 <2375353463@qq.com>
2020-08-29 00:36:46 +0800

    Money页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,43 @@
 <template>
-  <div>
-    Money.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+    </div>
     <Nav></Nav>
   </div>
 </template>
@@ -9,5 +46,15 @@
 export default {};
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+.nav-wrapper {
+  border: 1px solid lightgreen;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content {
+  flex-grow: 1;
+  overflow: auto;
+}
 </style>
