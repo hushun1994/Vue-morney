8e2308f4b94446bb665c3b1993bcf6778d565172
hushun1214 <2375353463@qq.com>
2020-08-29 22:36:37 +0800

    按照 vue-class-component 的文档，使用 props 失败

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -7,23 +7,41 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: "Types",
-  data() {
-    return {
-      type: "-", // '-'表示支出、 '+'表示收入
-    };
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+@Component({
+  props: {
+    propMessage: String,
   },
-  methods: {
-    selectType(type) {
-      if (type !== "-" && type !== "+") {
-        throw new Error("type is unknow");
-      }
-      this.type = type;
-    },
-  },
-};
+})
+export default class Types extends Vue {
+  type = "-";
+  helloMsg = "Hello" + this.propMessage;
+  selectType(type: string) {
+    if (type !== "-" && type !== "+") {
+      throw new Error("type is unknow");
+    }
+    this.type = type;
+  }
+}
+
+// export default {
+// name: "Types",
+// data() {
+//   return {
+//     type: "-", // '-'表示支出、 '+'表示收入
+//   };
+// },
+// methods: {
+//   selectType(type) {
+//     if (type !== "-" && type !== "+") {
+//       throw new Error("type is unknow");
+//     }
+//     this.type = type;
+//   },
+// },
+// };
 </script>
 
 <style lang="scss" scoped>
