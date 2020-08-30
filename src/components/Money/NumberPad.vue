8654059ef84fb8367dd777c639de98c7ead064c8
hushun1214 <2375353463@qq.com>
2020-08-30 11:05:12 +0800

    使用 v-model 代替 :value @input 绑定

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -26,7 +26,7 @@ import { Component } from "vue-property-decorator";
 
 @Component
 export default class NumberPad extends Vue {
-  output: string = "0";
+  output = "0";
   inputContent(event: MouseEvent) {
     const button = event.target as HTMLButtonElement;
     const input = button.textContent!;
