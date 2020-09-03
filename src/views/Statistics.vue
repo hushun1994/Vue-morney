22c03157619b21af1e310f4b1b3db27e4d016f3b
hushun1214 <2375353463@qq.com>
2020-09-03 17:28:45 +0800

    完成列表的CSS

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,8 +2,42 @@
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type" />
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval" />
+    <ol>
+      <li v-for="(group, index) in result" :key="index">
+        <h3 class="title">{{group.title}}</h3>
+        <ol>
+          <li v-for="item in group.items" :key="item.id" class="record">
+            <span>{{tagString(item.tags)}}</span>
+            <span class="record-notes">{{item.notes}}</span>
+            <span>¥{{item.amount}}</span>
+          </li>
+        </ol>
+      </li>
+    </ol>
   </Layout>
 </template>
+
+<style lang="scss" scoped>
+%item {
+  padding: 8px 16px;
+  line-height: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.title {
+  @extend %item;
+}
+.record {
+  @extend %item;
+  background: #fff;
+}
+.record-notes {
+  margin-right: auto;
+  margin-left: 16px;
+  color: #999;
+}
+</style>
 
 <script lang="ts">
 import Vue from "vue";
@@ -16,6 +50,30 @@ import recordTypeList from "@/constants/recordTypeList";
   components: { Tabs },
 })
 export default class Statistics extends Vue {
+  tagString(tags: Tag[]) {
+    return tags.length === 0 ? "无" : tags.join(",");
+  }
+  get recordList() {
+    return (this.$store.state as RootState).recordList;
+  }
+
+  get result() {
+    const { recordList } = this;
+    type HashTabValue = { title: string; items: RecordItem[] };
+    const hashTab: { [key: string]: HashTabValue } = {};
+    for (let i = 0; i < recordList.length; i++) {
+      const [date, time] = recordList[i].createAt!.split("T");
+      hashTab[date] = hashTab[date] || { title: date, items: [] };
+
+      hashTab[date].items.push(recordList[i]);
+    }
+    return hashTab;
+  }
+
+  created() {
+    this.$store.commit("fetchRecords");
+  }
+
   type = "-";
   interval = "day";
   intervalList = intervalList;
