2e99d51dc6f8b8a82acfc3509fbc9301ea57b726
hushun1214 <2375353463@qq.com>
2020-09-03 10:40:54 +0800

    在 Money.vue 里面使用 Tabs 组件替换掉 Types 组件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout class-prefix="layout">
     <NumberPad @submit="saveRecord" :value.sync="record.amount" />
-    <Types :value.sync="record.type" />
+    <Tabs :data-source="recordTypeList" :value.sync="record.type" />
     <div class="notes">
       <FormItem fieldName="备注" placeholder="请在这里输入" @update:value="onUpdateNotes" />
     </div>
@@ -12,18 +12,21 @@
 <script lang="ts">
 import Vue from "vue";
 import NumberPad from "@/components/Money/NumberPad.vue";
-import Types from "@/components/Money/Types.vue";
 import FormItem from "@/components/Money/FormItem.vue";
 import Tags from "@/components/Money/Tags.vue";
+import Tabs from "@/components/Tabs.vue";
 import { Component } from "vue-property-decorator";
+import recordTypeList from "@/constants/recordTypeList";
 
 @Component({
-  components: { NumberPad, Types, FormItem, Tags },
+  components: { NumberPad, Tabs, FormItem, Tags },
 })
 export default class Money extends Vue {
   get recordList() {
     return this.$store.state.recordList;
   }
+
+  recordTypeList = recordTypeList;
 
   record: RecordItem = {
     tags: [],
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,31 +1,25 @@
 <template>
   <Layout>
-    <Tabs class-prefix="type" :data-source="typeList" :value.sync="type" />
+    <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type" />
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval" />
   </Layout>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-// import Types from "@/components/Money/Types.vue";
 import Tabs from "@/components/Tabs.vue";
 import { Component, Prop } from "vue-property-decorator";
+import intervalList from "@/constants/intervalList";
+import recordTypeList from "@/constants/recordTypeList";
 
 @Component({
   components: { Tabs },
 })
 export default class Statistics extends Vue {
   type = "-";
-  intervalList = [
-    { text: "按天", value: "day" },
-    { text: "按周", value: "week" },
-    { text: "按月", value: "month" },
-  ];
-  interval = "day";
-  typeList = [
-    { text: "支出", value: "-" },
-    { text: "收入", value: "+" },
-  ];
+  interval = "按天";
+  intervalList = intervalList;
+  recordTypeList = recordTypeList;
 }
 </script>
 
